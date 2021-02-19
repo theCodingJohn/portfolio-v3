a53de9bd42e4c6077c0b8b6d3937151855832664
John Carlo Cunanan <codingjohn@outlook.com>
2021-02-19 22:18:59 +0800

    add project card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,15 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 // components 
 import RocketHeading from "./RocketHeading"
+import ProjectCard from "./ProjectCard"
+
+// images 
+import photosnap from "../assets/desktop/photosnap.png"
+import photo2 from "../assets/desktop/photo2.png"
+import photo3 from "../assets/desktop/photo3.png"
+import photo4 from "../assets/desktop/photo4.png"
+import photo5 from "../assets/desktop/photo5.png"
+import photo6 from "../assets/desktop/photo6.png"
 
 const Projects = () => {
 
@@ -39,7 +48,14 @@ const Projects = () => {
         </div>
       </div>
       <div className="row row2">
-        <div className="col col1"></div>
+        <div className="col col1">
+          <ProjectCard attribute={{image: `${photosnap}`}}/>
+          <ProjectCard attribute={{image: `${photo2}`}}/>
+          <ProjectCard attribute={{image: `${photo3}`}}/>
+          <ProjectCard attribute={{image: `${photo4}`}}/>
+          <ProjectCard attribute={{image: `${photo5}`}}/>
+          <ProjectCard attribute={{image: `${photo6}`}}/>
+        </div>
       </div>
       <div className="row row3">
         <div className="col col1"></div>
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Hero from "./Hero";
 import About from "./About";
 import Projects from "./Projects";
 import RocketHeading from "./RocketHeading";
+import ProjectCard from "./ProjectCard";
 
-export { Header, Hero, About, Projects, RocketHeading };
+export { Header, Hero, About, Projects, RocketHeading, ProjectCard };
