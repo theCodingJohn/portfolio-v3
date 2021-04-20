e26b4aa6daa95ece72e25cdfc7e0c132bbaef4d3
John Carlo Cunanan <codingjohn@outlook.com>
2021-04-20 15:33:51 +0800

    Add footer

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import style from "../styles/Home.module.scss";
 
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
@@ -15,8 +16,47 @@ const HomePage = () => {
       <main className={style.home}>
         <Hero />
       </main>
+      <Footer />
     </>
   );
 };
+
+// const getToken = async () => {
+//   try {
+//     const params = new URLSearchParams();
+//     params.append("grant_type", "refresh_token");
+//     params.append("refresh_token", `${process.env.REACT_APP_REFRESH_TOKEN}`);
+
+//     const config = {
+//       headers: {
+//         authorization: `Basic ${process.env.REACT_APP_SPOTIFY_AUTHORIZATION}`,
+//         "Content-Type": "application/x-www-form-urlencoded",
+//       },
+//     };
+//     const res = await axios.post(
+//       "https://accounts.spotify.com/api/token",
+//       params,
+//       config
+//     );
+//     return res.data.access_token;
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+// export const getStaticProps = async (context) => {
+//   const token = await getToken();
+//   const config = { headers: { authorization: `Bearer ${token}` } };
+//   const res = await axios.get(
+//     "https://api.spotify.com/v1/me/player/currently-playing",
+//     config
+//   );
+
+//   return {
+//     props: {
+//       nowPlaying: res.data.item,
+//     },
+//   };
+// };
 
 export default HomePage;
